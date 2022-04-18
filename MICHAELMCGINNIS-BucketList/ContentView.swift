dae493d35e2dffc2afa3875b6b66a831fd5269ce
Rukosa <mwmcginnis490@gmail.com>
2022-04-17 22:14:13 -0700

    Initial Commit

---
+++ MICHAELMCGINNIS-BucketList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MICHAELMCGINNIS-BucketList
+//
+//  Created by Michael Mcginnis on 4/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MICHAELMCGINNIS-BucketList/MICHAELMCGINNIS_BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MICHAELMCGINNIS_BucketListApp.swift
+//  MICHAELMCGINNIS-BucketList
+//
+//  Created by Michael Mcginnis on 4/17/22.
+//
+
+import SwiftUI
+
+@main
+struct MICHAELMCGINNIS_BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
